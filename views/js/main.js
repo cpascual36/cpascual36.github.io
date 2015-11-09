5e0d8c695b732f2b62cea37651f536f9e66b51f0
cpascual36 <cpascual36@hotmail.com>
2015-11-09 18:45:51 -0500

    change number of pizzas to 75

--- views/js/main.js
+++ views/js/main.js
@@ -510,7 +510,7 @@ function updatePositions() {
   var phase;
 
   for (var i = 0; i < items.length; i++) {
-    phase = Math.sin(top + i % 5);
+    phase = Math.sin(top + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
@@ -532,7 +532,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < 75; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
