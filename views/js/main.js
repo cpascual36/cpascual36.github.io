cac11505bf1eccebede412b3f31ba2c0d98decfc
cpascual36 <cpascual36@hotmail.com>
2015-11-09 22:12:26 -0500

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -513,7 +513,7 @@ function updatePositions() {
     phase = Math.sin(top + (i % 5));
      items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
-
+}
   // User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
   window.performance.mark("mark_end_frame");
