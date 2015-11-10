35af5c1dc3021d4baabdc9426ae48ba216378be7
cpascual36 <cpascual36@hotmail.com>
2015-11-09 21:53:48 -0500

    width

--- views/js/main.js
+++ views/js/main.js
@@ -537,7 +537,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.className = 'mover';
     elem.src = "images/pizza.png";
    // elem.style.height = "74px";
-   // elem.style.width = "73.333px";
+   elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
